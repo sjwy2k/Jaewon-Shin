c297d680060ffbc1aff4520e4020bca19b3b1842
Jaewon Shin <sjwy2k@gmail.com>
2017-10-23 17:21:23 +0900

    171023_STSCommit

--- BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWeb/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -10,31 +10,32 @@ import com.springbook.biz.board.BoardVO;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
+	
 	@Autowired
 	private BoardDAO boardDAO;
 	
 	@Override
-	public void insertBoard(BoardVO vo) {
+	public void insertBoard(BoardVO vo) {		
 		boardDAO.insertBoard(vo);
 	}
 	
 	@Override
-	public void updateBoard(BoardVO vo) {
+	public void updateBoard(BoardVO vo) {		
 		boardDAO.updateBoard(vo);
 	}
 	
 	@Override
-	public void deleteBoard(BoardVO vo) {
+	public void deleteBoard(BoardVO vo) {		
 		boardDAO.deleteBoard(vo);
 	}
 	
 	@Override
-	public BoardVO getBoard(BoardVO vo) {
+	public BoardVO getBoard(BoardVO vo) {		
 		return boardDAO.getBoard(vo);
 	}
 	
 	@Override
-	public List<BoardVO> getBoardList(BoardVO vo) {
+	public List<BoardVO> getBoardList(BoardVO vo) {		
 		return boardDAO.getBoardList(vo);
 	}
 }
